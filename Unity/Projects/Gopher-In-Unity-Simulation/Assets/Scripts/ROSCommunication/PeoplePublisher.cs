efaa357e1d64da5b800570e9a497701deb18aac3
Denny Boby <dennyboby123@gmail.com>
2022-04-16 17:19:00 -0400

    [WIP] added people publisher script

---
+++ Unity/Projects/Gopher-In-Unity-Simulation/Assets/Scripts/ROSCommunication/PeoplePublisher.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Unity.Robotics.Core;
+using Unity.Robotics.ROSTCPConnector;
+using Unity.Robotics.ROSTCPConnector.ROSGeometry;
+using RosMessageTypes.Std;
+using RosMessageTypes.Geometry;
+
+public class PeoplePublisher : MonoBehaviour
+{
+    // ROS Connector
+    private ROSConnection ros;
+    // Variables required for ROS communication
+    public string poseStampedTopicName = "people";
+    
+    // Transform
+    public Transform publishedTransform;
+
+    // Message
+    private PoseStampedMsg poseStamped;
+    private string frameID = "map";
+    public float publishRate = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get ROS connection static instance
+        ros = ROSConnection.GetOrCreateInstance();
+        ros.RegisterPublisher<PoseStampedMsg>(poseStampedTopicName);
+
+        // Initialize message
+        poseStamped = new PoseStampedMsg
+        {
+            header = new HeaderMsg(Clock.GetCount(),
+                                   new TimeStamp(Clock.time), frameID)
+        };
+
+        InvokeRepeating("PublishPoseStamped", 1f, 1f/publishRate);
+    }
+
+    private void PublishPoseStamped()
+    {
+        poseStamped.header = new HeaderMsg(Clock.GetCount(), 
+                                           new TimeStamp(Clock.time), frameID);
+
+        poseStamped.pose.position = publishedTransform.position.To<FLU>();
+        poseStamped.pose.orientation = publishedTransform.rotation.To<FLU>();
+
+        ros.Publish(poseStampedTopicName, poseStamped);
+    }
+}
