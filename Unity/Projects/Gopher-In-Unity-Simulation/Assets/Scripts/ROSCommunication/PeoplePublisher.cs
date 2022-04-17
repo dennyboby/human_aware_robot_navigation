2b756d06a8fa51fc1bab857e7cf2427315fada57
Denny Boby <dennyboby123@gmail.com>
2022-04-17 00:36:25 -0400

    [WIP] peoplepublisher almost working

--- Unity/Projects/Gopher-In-Unity-Simulation/Assets/Scripts/ROSCommunication/PeoplePublisher.cs
+++ Unity/Projects/Gopher-In-Unity-Simulation/Assets/Scripts/ROSCommunication/PeoplePublisher.cs
@@ -7,47 +7,56 @@ using Unity.Robotics.ROSTCPConnector;
 using Unity.Robotics.ROSTCPConnector.ROSGeometry;
 using RosMessageTypes.Std;
 using RosMessageTypes.Geometry;
+using RosMessageTypes.People;
 
 public class PeoplePublisher : MonoBehaviour
 {
     // ROS Connector
     private ROSConnection ros;
     // Variables required for ROS communication
-    public string poseStampedTopicName = "people";
-    
+    public string peopleTopicName = "people";
     // Transform
     public Transform publishedTransform;
-
     // Message
-    private PoseStampedMsg poseStamped;
+    private PeopleMsg peopleMsg;
+    // private PersonMsg personMsg;
     private string frameID = "map";
     public float publishRate = 10f;
+    public CharacterWalk script;
 
     // Start is called before the first frame update
     void Start()
     {
         // Get ROS connection static instance
         ros = ROSConnection.GetOrCreateInstance();
-        ros.RegisterPublisher<PoseStampedMsg>(poseStampedTopicName);
+        ros.RegisterPublisher<PeopleMsg>(peopleTopicName);
 
         // Initialize message
-        poseStamped = new PoseStampedMsg
+        peopleMsg = new PeopleMsg
         {
             header = new HeaderMsg(Clock.GetCount(),
                                    new TimeStamp(Clock.time), frameID)
         };
-
         InvokeRepeating("PublishPoseStamped", 1f, 1f/publishRate);
     }
 
     private void PublishPoseStamped()
     {
-        poseStamped.header = new HeaderMsg(Clock.GetCount(), 
+        peopleMsg.header = new HeaderMsg(Clock.GetCount(), 
                                            new TimeStamp(Clock.time), frameID);
+        var orientation = publishedTransform.eulerAngles;
+        var personMsg = new PersonMsg
+        {
+            name = "person",
+            position = publishedTransform.position.To<FLU>(),
+            velocity = new PointMsg(script.speed*Mathf.Sin(orientation[1]*Mathf.Deg2Rad),
+                    script.speed*Mathf.Cos(orientation[1]*Mathf.Deg2Rad),0),
+            reliability = 1.0,
+            tagnames = new string[] { "tag1", "tag2" },
+            tags = new string[] { "tag1", "tag2" }
+        };
+        peopleMsg.people = new PersonMsg[] { personMsg };
 
-        poseStamped.pose.position = publishedTransform.position.To<FLU>();
-        poseStamped.pose.orientation = publishedTransform.rotation.To<FLU>();
-
-        ros.Publish(poseStampedTopicName, poseStamped);
+        ros.Publish(peopleTopicName, peopleMsg);
     }
 }
